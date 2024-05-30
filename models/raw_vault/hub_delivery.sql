7646f77ad9824f431cf0886cf83605c3fa0e3291
thh1118 <130691422+thh6282@users.noreply.github.com>
2024-05-30 23:09:36 +0700

    Update hub_delivery.sql

--- models/raw_vault/hub_delivery.sql
+++ models/raw_vault/hub_delivery.sql
@@ -7,11 +7,16 @@
         views_enabled=False,
     )
 }}    
-
+with sqlmain as (
 select
     to_hex(md5(to_utf8('App_Data' || '_' || COALESCE(cast(DeliveryNumber as varchar), '')))) as delivery_hash_key,
     load_date as load_date,
     'App_Data' || '_' || 'MPLDeliveryManager' as record_source,
     DeliveryNumber as delivery_id
 from staging.stg_appdata__mpldeliverymanager
-where 1=1 
+)
+select * from sqlmain a
+
+{% if is_cremental()%}
+    {where not EXISTS (select 1 from {{ this }} b where a.delivery_hash_key= b.delivery_hash_key)}
+{% endif %}
